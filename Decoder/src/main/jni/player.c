e00d1dc5b81827d41a76f345147f099c65621e34
luckmerlin <676046361>
2019-12-16 07:50:39 +0800

    Modify native player

--- Decoder/src/main/jni/player.c
+++ Decoder/src/main/jni/player.c
@@ -132,45 +132,6 @@ Java_com_merlin_player_Player_play(JNIEnv *env,jobject type,jstring path,jfloat 
     return JNI_TRUE;
 }
 
-//static inline int readNextFrame(MP3FileHandle* mp3){
-//    do{
-//        if(mp3->stream.buffer == 0 || mp3->stream.error == MAD_ERROR_BUFLEN){
-//            int inputBufferSize = 0;
-//            if(mp3->stream.next_frame != 0){
-//                int leftOver = mp3->stream.bufend - mp3->stream.next_frame;
-//                int i;
-//                for(i= 0;i<leftOver;i++){
-//                    mp3->inputBuffer[i] = mp3->stream.next_frame[i];
-//                }
-//                int readBytes = fileRead(mp3->fd, mp3->inputBuffer+leftOver,INPUT_BUFFER_SIZE-leftOver);
-//                if(readBytes == 0)
-//                    return 0;
-//                inputBufferSize = leftOver + readBytes;
-//                mp3 ->fileStartPos += readBytes;
-//            }else{
-//                int readBytes = fileRead(mp3->fd,mp3->inputBuffer,INPUT_BUFFER_SIZE);
-//                if(readBytes == 0)
-//                    return 0;
-//                inputBufferSize = readBytes;
-//                mp3 ->fileStartPos += readBytes;
-//            }
-//            mad_stream_buffer(&mp3->stream,mp3->inputBuffer,inputBufferSize);
-//            mp3->stream.error = MAD_ERROR_NONE;
-//        }
-//        if(mad_frame_decode(&mp3->frame,&mp3->stream)){
-//            if( mp3->stream.error == MAD_ERROR_BUFLEN ||(MAD_RECOVERABLE(mp3->stream.error)))
-//                continue;
-//            else
-//                return 0;
-//        }else
-//            break;
-//    }while(1);
-//    mad_timer_add(&mp3->timer, mp3->frame.header.duration);
-//    mad_synth_frame(&mp3->synth,&mp3->frame);
-//    mp3->leftSamples = mp3->synth.pcm.length;
-//    mp3->offset = 0;
-//    return -1;
-//}
 JNIEXPORT jboolean JNICALL
 Java_com_merlin_player_Player_playBytes(JNIEnv *env, jobject thiz, jbyteArray data, jint offset,
                                         jint length, jboolean reset) {
