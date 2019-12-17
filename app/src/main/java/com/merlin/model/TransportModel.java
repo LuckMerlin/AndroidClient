880f955ea073bcdb52b2b545ca2810fa2e411e47
LuckMerlin <676046361@qq.com>
2019-12-17 18:02:11 +0800

    增加下载activity

--- app/src/main/java/com/merlin/model/TransportModel.java
+++ app/src/main/java/com/merlin/model/TransportModel.java
@@ -3,23 +3,28 @@ package com.merlin.model;
 import android.content.Context;
 import android.view.View;
 
+import androidx.lifecycle.ViewModel;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.merlin.adapter.TransportingAdapter;
+import com.merlin.client.R;
+import com.merlin.debug.Debug;
 import com.merlin.transport.Transport;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TransportModel extends DataListModel {
+public class TransportModel extends BaseModel {
 
     public TransportModel(Context context){
-        super(context,new TransportingAdapter(),new LinearLayoutManager(context));
+        super(context);
     }
 
 
     @Override
     protected void onViewAttached(View root) {
+        RecyclerView rv=findViewById(R.id.activity_transporting_listRV, RecyclerView.class);
         List<Transport> list=new ArrayList<>();
         Transport transport=new Transport("我们都是好孩子.mp3","/volume1/","Good",System.currentTimeMillis());
         list.add(transport);
@@ -27,6 +32,6 @@ public class TransportModel extends DataListModel {
         list.add(transport);
         transport=new Transport("帝发誓.mp3","/volume1/","Good",System.currentTimeMillis());
         list.add(transport);
-        setData(list,true);
+        Debug.D(getClass(),"@@@@@@@@@@@@@@@@@@@ "+rv);
     }
 }
